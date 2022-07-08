70111fef3d19b3b204ff2eb3266a1eb9025358d5
halitov-r-k <halitov.r.k@gmail.com>
2022-07-08 09:00:38 +0500

    2022-07-08

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,29 @@
-public class Programm {
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Программа");
+
+        byte[] input = {22, 2, 3, 4};
+        InputStream inputStream = new ByteArrayInputStream(input);
+        try {
+            System.out.println(sumOfStream(inputStream));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static public int sumOfStream(InputStream inputStream) throws IOException {
+       int sum = 0;
+       int b;
+        try {
+            while ((b = inputStream.read()) != -1) {
+                sum += b;
+            }
+        } catch (IOException e) {
+            throw new IOException();
+        }
+        return sum;
+    }
 }
